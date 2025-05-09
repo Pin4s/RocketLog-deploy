7e133aa77a7f3fa0c2310966d3c5eeaa27a559a2
Pedro Pinas <joaopedrolp2204@gmail.com>
2025-05-09 11:02:16 -0300

    Validating data with zod

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,9 +1,22 @@
 import { Request, Response } from "express";
+import { hash } from "bcrypt"
+import { z } from "zod"
 
 class UserController {
 
-    create(req: Request, res: Response) {
-        return res.json({ message: "ok!" })
+    async create(req: Request, res: Response) {
+
+        const bodySchema = z.object({
+            name: z.string().trim().min(3),
+            email: z.string().email(),
+            password: z.string().min(6)
+        })
+
+        const { name, email, password } = bodySchema.parse(req.body)
+
+        const hashedPassword = await hash(password, 8)
+
+        return res.json({ message: "ok!", hashedPassword })
     }
 }
 
